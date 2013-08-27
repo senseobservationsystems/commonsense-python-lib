6035c93a9aaa2eb6ca156426ef96a29420757b64
ted <ted@sense-os.nl>
2013-08-27 16:04:17 +0200

    Updated the version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='senseapi',
-      version = '0.3.0',
+      version = '0.4.0',
       packages = [''],
       install_requires = ['oauth>=1.0.1'],
       author = "Sense Observation Systems",
