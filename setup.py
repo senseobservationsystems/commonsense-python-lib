951be55d7ec768e668f0e7de26fc1dee0faefd95
Low Kian Seong <kianseong@gmail.com>
2014-08-16 19:16:45 +0800

    adding metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
-setup(name = 'senseapi',
-      version = '0.5.12',
-      packages = [''],
-      install_requires = ['oauth>=1.0.1'],
-      author = "Sense Observation Systems",
-      author_email = "info@sense-os.nl",
-      license = "Apache 2.0",
-      keywords = "sense sensor api",
-      url = "https://github.com/senseobservationsystems/commonsense-python-lib"
+setup(name='senseapi',
+      version='0.5.12',
+      packages=[''],
+      install_requires=['oauth>=1.0.1'],
+      author="Sense Observation Systems",
+      author_email="info@sense-os.nl",
+      description='Library for using CommonSense API in Python applications',
+      license="Apache 2.0",
+      keywords="sense sensor api",
+      url="https://github.com/senseobservationsystems/commonsense-python-lib"
       )
